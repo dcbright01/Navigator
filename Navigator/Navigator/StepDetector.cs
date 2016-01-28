b8f49cecb5db0fff460512bee789245b94457e9b
Pedram Amirkhalili <fireyp@gmail.com>
2016-01-28 17:57:27 +0000

    Added Step Detector Class

---
+++ Navigator/Navigator/StepDetector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigator
+{
+    class StepDetector
+    {
+        public StepDetector()
+        {
+            //constructor
+        }
+
+        public void reset()
+        {
+            //reset all local variables
+        }
+
+        public void passValue()
+        {
+            //receive input from
+        }
+    }
+}

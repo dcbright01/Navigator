4bd32d47add8cb04b39ffd25544eb7661db5cbe5
Pedram Amirkhalili <fireyp@gmail.com>
2016-02-11 20:07:59 +0000

    Added the algorithms to the collision class file, currently all untested.

--- Navigator/Navigator/Collision.cs
+++ Navigator/Navigator/Collision.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Navigator.Pathfinding;
 
 namespace Navigator
 {
+    //enum for sensor type
+    enum Sensor
+    {
+        Accel,
+        Gryo,
+        Mag
+    };
 
+    //delegates to define the output for events
     public delegate void CollisionHandler(float realX, float realY);
     public delegate void HeadingHandler(int nHeading);
 
     class Collision
     {
+
         //Values that are being tracked
         private Tuple<float, float> realPosition;
         private Tuple<int, int> nearestGraphNode;
-        private int heading; 
+        private int heading;
+
+        //Other values
+        private const float strideLength = 3.0f;
+        private const int searchDistance = 6;
+
+        //path holder
+        Queue<Tuple<int, int>> graphPath;
+
+        //graph information
+        Graph g;
         
         //Require interfaces that values are passed to
         //Additional Interfaces added here
@@ -30,29 +50,30 @@ namespace Navigator
         public event CollisionHandler validMove;
         public event HeadingHandler newHeading;
 
-        public Collision()
+        public Collision(Graph nGraph)
         {
             //constructor
+            g = nGraph;
         }
 
-        public void giveStartingLocation(float startX, float startY)
+        public void GiveStartingLocation(float startX, float startY)
         {
             realPosition = Tuple.Create(startX, startY);
             nearestNode();
         }
 
-        public void passSensorReadings(double accelX, double accelY, double accelZ, double gyroX, double gyroY, double gyroZ, double magX, double magY, double magZ)
+        public void PassSensorReadings(Sensor s, double xVal, double yVal, double zVal)
         {
             //give these values to whatever interfaces/components require
         }
 
-        public virtual void validMoveMade()
+        public virtual void ValidMoveMade()
         {
             if (validMove != null)
                 validMove(realPosition.Item1, realPosition.Item2);
         }
 
-        public virtual void headingChange()
+        public virtual void HeadingChange()
         {
             if (newHeading != null)
                 newHeading(heading);
@@ -60,21 +81,96 @@ namespace Navigator
 
         private void nearestNode()
         {
-            //work out the nearest node from changed real position
+            Tuple<int, int> tempNode = new Tuple<int, int>(-1, -1);
+            double distanceFromTempToReal = -1;
+            double a, b, newDistance;
+
+            string nodeCoords;
+            
+            //there is a more effecient search that would spiral outwards to a set point before searching the corners of the sqaure that this gets, can implement if need be
+            for(int x = (int) realPosition.Item1 - searchDistance; x <= realPosition.Item1 + searchDistance; x++)
+            {
+                for(int y = (int) realPosition.Item2 - searchDistance; y <= realPosition.Item2 + searchDistance; y++)
+                {
+                    nodeCoords = x.ToString() + "-" + y.ToString();
+
+                    string nodeCheck = g.Vertices.FirstOrDefault(node => node == nodeCoords);
+                    if(nodeCheck != null)
+                    {
+                        a = y - realPosition.Item2;
+                        b = x - realPosition.Item1;
+                        newDistance = Math.Sqrt((a * a) + (b * b));
+                        if (distanceFromTempToReal == -1 || newDistance < distanceFromTempToReal)
+                        {//first node come across is set as the tempNode or if newDistance is smaller
+                            tempNode = Tuple.Create(x, y);
+                            distanceFromTempToReal = newDistance;
+                        } //shouldn't need to deal with case where you are the same distance from multiple nodes, as it won't really effect navigation and rules, if required can be added
+                    }                                        
+                }
+            }
+
+            string start, end;
+
+            //case where this is the initial position, figure out how for initial to avoid wall hopping
+            if(nearestGraphNode == null)
+            {
+
+            } else if(!nearestGraphNode.Equals(tempNode)) // for the case where its not the initial and the previous value is different from current
+            {
+                start = nearestGraphNode.Item1.ToString() + "-" + nearestGraphNode.Item2.ToString();
+                end = tempNode.Item1.ToString() + "-" + tempNode.Item2.ToString();
+                var path = g.FindPath(start, end);  
+                if(path.Count < 3)
+                {
+                    if(graphPath.Count == 5)
+                    {
+                        graphPath.Dequeue();
+                    }
+                    graphPath.Enqueue(tempNode);
+                    nearestGraphNode = tempNode;
+                } 
+            }
         }
 
         //replace with StepDetection Event trigger
         private void testStepTrigger()
         {
-            //if move valid update positions, trigger validMove event and recalculate the nearestNode
-            //else do nothing
+            string start, end;
+
+            int xHeading = (heading - 90) * (int) (Math.PI / 180);
+            float x = realPosition.Item1 + strideLength * (float) Math.Cos(xHeading);
+            float y = realPosition.Item2 + strideLength * (float) Math.Sin(xHeading);
+            Tuple<float, float> newPosition = new Tuple<float, float>(x, y);
+
+            Tuple<float, float> realHolder = realPosition;
+            Tuple<int, int> nearestHolder = nearestGraphNode;
+
+            realPosition = newPosition;
+            nearestNode();
+            if (!nearestHolder.Equals(nearestGraphNode))
+            {
+                start = nearestGraphNode.Item1.ToString() + "-" + nearestGraphNode.Item2.ToString();
+                end = nearestHolder.Item1.ToString() + "-" + nearestHolder.Item2.ToString();
+                var path = g.FindPath(start, end);
+                if(path.Count > 2)
+                {
+                    realPosition = realHolder;
+                    nearestGraphNode = nearestHolder;
+                } else
+                {
+                    ValidMoveMade();
+                }
+            } else
+            {
+                ValidMoveMade();
+            }
         }
 
         //replace with Heading Event trigger
         private void testHeadingTrigger(int nHeading)
         {
             heading = nHeading;
-            headingChange();
+            HeadingChange();
         }
     }
 }

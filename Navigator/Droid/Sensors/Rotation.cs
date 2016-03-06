6eae55797b3dd99cf9a65edd338316ad9c4edd0f
Varun <vg.blazingdevil@gmail.com>
2016-03-06 16:22:45 +0000

    Added low pass filter for Azimuth and also included the use of 'RemapCoordinates'. Azimuth still changes when tilting phone, but overall it seems to be more stable than before.

--- Navigator/Droid/Sensors/Rotation.cs
+++ Navigator/Droid/Sensors/Rotation.cs
@@ -3,54 +3,63 @@ using Android.Hardware;
 
 namespace Navigator.Droid.Sensors
 {
-    public class Rotation : SensorProcessorBase<double>
-    {
-        private float[] _geomagnetic;
-        private float[] _gravity;
+	public class Rotation : SensorProcessorBase<double>
+	{
+		private float[] _geomagnetic = new float[3];
+		private float[] _gravity = new float[3];
 
-        public Rotation(SensorManager manager) : base(manager)
-        {
-            AcceptedSensorTypes.Add(SensorType.MagneticField);
-            AcceptedSensorTypes.Add(SensorType.Gravity);
-        }
+		public Rotation(SensorManager manager) : base(manager)
+		{
+			AcceptedSensorTypes.Add(SensorType.MagneticField);
+			AcceptedSensorTypes.Add(SensorType.Gravity);
+		}
 
-        public override void SensorChangedProcess(SensorEvent e)
-        {
-            switch (e.Sensor.Type)
-            {
-                case SensorType.MagneticField:
-                    _geomagnetic = e.Values.ToArray();
-                    break;
-                case SensorType.Gravity:
-                    _gravity = e.Values.ToArray();
-                    break;
-            }
+		public override void SensorChangedProcess(SensorEvent e)
+		{
+			float alpha = 0.97f;
 
-            if (_gravity != null && _geomagnetic != null)
-            {
-                var R = new float[9];
-                var I = new float[9];
-                var success = SensorManager.GetRotationMatrix(R, I, _gravity, _geomagnetic);
-                if (success)
-                {
-                    var orientation = new float[3];
-                    SensorManager.GetOrientation(R, orientation);
-                    Value = orientation[0]; // orientation contains: azimut, pitch and roll
-                    ValueHistory.Enqueue(Value);
-                    if (OnValueChanged != null)
-                    {
-                        OnValueChanged(Value);
-                    }
-                }
-            }
-        }
+			switch (e.Sensor.Type)
+			{
+			case SensorType.MagneticField:
+				_geomagnetic [0] = alpha * _geomagnetic [0] + (1 - alpha) * e.Values [0];
+				_geomagnetic [1] = alpha * _geomagnetic [1] + (1 - alpha) * e.Values [1];
+				_geomagnetic [2] = alpha * _geomagnetic [2] + (1 - alpha) * e.Values [2];
+				break;
+			case SensorType.Gravity:
+				_gravity [0] = alpha * _gravity [0] + (1 - alpha) * e.Values [0];
+				_gravity [1] = alpha * _gravity [1] + (1 - alpha) * e.Values [1];
+				_gravity [2] = alpha * _gravity [2] + (1 - alpha) * e.Values [2];
+				break;
+			}
 
-        #region <Event stuff>
+			if (_gravity != null && _geomagnetic != null)
+			{
+				var R = new float[9];
+				var I = new float[9];
+				var success = SensorManager.GetRotationMatrix(R, I, _gravity, _geomagnetic);
+				if (success)
+				{
+					float[] remappedRotationMatrix = new float[9];
+					SensorManager.RemapCoordinateSystem (R, Android.Hardware.Axis.X, Android.Hardware.Axis.Z, remappedRotationMatrix);
 
-        public event OnValueChangedHandler OnValueChanged;
+					var orientation = new float[3];
+					SensorManager.GetOrientation(remappedRotationMatrix, orientation);
+					Value = orientation[0]; // orientation contains: azimut, pitch and roll
+					ValueHistory.Enqueue(Value);
+					if (OnValueChanged != null)
+					{
+						OnValueChanged(Value);
+					}
+				}
+			}
+		}
 
-        public delegate void OnValueChangedHandler(double value);
+		#region <Event stuff>
 
-        #endregion
-    }
+		public event OnValueChangedHandler OnValueChanged;
+
+		public delegate void OnValueChangedHandler(double value);
+
+		#endregion
+	}
 }
